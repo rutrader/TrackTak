675b060701cc4769c8f7fc423c5ce3d11eba839c
Martin Dawson <MDawson@marketaxess.com>
2020-11-13 19:24:11 +0000

    change background to purple

--- ui/src/App.js
+++ ui/src/App.js
@@ -6,7 +6,7 @@ import rebassTheme from './rebassTheme';
 const GlobalStyle = createGlobalStyle`
   * { box-sizing: border-box; }
   body { margin: 0; min-height: 100%; height: 100%; }
-  #app { height: inherit; > div { height: 100%; } }
+  #root { height: inherit; > div { height: 100%; } }
   html { height: 100%; }
   a { color: inherit; text-decoration: none; }
   ul { list-style: none; margin: 0; padding: 0; }
--- ui/src/landingPage/LandingPage.jsx
+++ ui/src/landingPage/LandingPage.jsx
@@ -1,45 +1,28 @@
 import React from "react";
-import { Container, Text, Absolute, Relative, Image} from "rebass";
+import { Container, Text, Absolute, Relative} from "rebass";
 import { Box, Flex } from "grid-styled";
 import styled from "styled-components";
-import { ReactComponent as BackgroundBlue } from "../icons/backgroundBlue.svg";
+import { ReactComponent as BackgroundPurple } from "../icons/backgroundPurple.svg";
 import { ReactComponent as Check }  from "../icons/check.svg";
-import StepNumber from "./StepNumber";
-import { ReactComponent as DotLines } from "../icons/dotLines.svg";
-import Hide from "../utils/Hide";
-import StepOne from "../icons/step1.jpg";
-import StepTwo from "../icons/step2.jpg";
-import StepThree from "../icons/step3.jpg";
 import Button from "../utils/Button";
 import SubscribeMailingList from "./SubscribeMailingList";
 
 const GreenCheck = styled(Check)`
   margin-right: 10px;
-  min-width: 20px;
+  min-width: 30px;
+  min-height: 30px;
   fill: ${({ theme }) => theme.colors.primary}
 `;
 
-const StyledStepFlex = styled(Flex)`
-  @media screen and (max-width: 768px) {
-    > svg {
-      width: 100%;
-    }
-  }
-`;
-
-const StepFlex = ({ ...props }) => <StyledStepFlex {...props} mb={[40, 40, 0]} justifyContent="space-between" alignItems={["center", "center", "stretch"]} flexDirection={["column", "column", "row"]} />;
-const StepContainer = ({ ...props }) => <Box {...props} width={[1, 1, 370]} mt={[0, 0, 44]} mb={[30, 30, 0]} order={[-1, -1, 0]} />;
-const StepHeading = ({ ...props }) => <Text {...props} textAlign={["center", "center", "left"]} mb={35} mt={25} fontSize={30} />;
-const StepContent = ({ ...props }) => <Box is="ul" {...props} mx={["auto", "auto", "none"]} fontSize={18} />;
+const StepContainer = ({ ...props }) => <Box {...props} width={[1, 1, 500]} mt={[0, 0, 44]} mb={[30, 30, 0]} order={[-1, -1, 0]} />;
 const StepItem = ({ children, ...props }) => (
   <Box is="li" width={["initial", "60%", "100%"]} mx={["5%", "20%", 0]} my={12} {...props}>
-    <Flex>
-      <GreenCheck width={20} height={20} />
+    <Flex alignItems="center">
+      <GreenCheck />
       <Text fontSize={21}>{children}</Text>
     </Flex>
   </Box>
 );
-const DotLinesImage = ({ ...props }) => <DotLines {...props} width="66%" style={{ margin: "0 auto", display: "block", ...props.style }}  />;
 const GetStartedMailingList = () => (
   <SubscribeMailingList
     subscribeText="Get Started"
@@ -59,63 +42,33 @@ const GetStartedMailingList = () => (
 );
 
 const LandingPage = () => (
-  <Relative>
-    <Absolute style={{ width: "100%", height: 435 }} zIndex={-1}>
-      <BackgroundBlue height="100%" width="100%" />
-    </Absolute>
-    <Container pt={150} maxWidth={1100}>
-      <Text textAlign="center" mb={34} fontSize={24} color="white">
-        Enter your email to get the chrome extension
-      </Text>
-      <GetStartedMailingList />
-      <Text textAlign="center" fontSize={[40, 60]} mt={165}>
-        How It Works
-      </Text>
-      <Box mt={50} mb={300}>
-        <StepFlex>
-          <Image
-            src={StepOne}
-          />
+  <Relative style={{ height: '100%' }} >
+    <Container pt={20} maxWidth={1100} style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <Box>
+        <Text fontSize={[30, 40]} mt={10}>
+          tracktak
+        </Text>
+        <Text fontSize={[40, 60]} mt={30} color="#292929">
+          Goodbye, spreadsheets.
+          <Box>Hello, automated Discounted Cash Flows.</Box>
+        </Text>
+        <Box mt={50}>
           <StepContainer>
-            <StepNumber>1</StepNumber>
-            <StepHeading>Install the chrome onboardor extension</StepHeading>
-            <StepContent>
-              <StepItem>Onboard new members through GitHub. No external software.</StepItem>
-            </StepContent>
+            <StepItem>Fully automates your models inputs based on historical data.</StepItem>
+            <StepItem>Store all of your model variations in one place instead of 100’s of spreadsheets.</StepItem>
+            <StepItem>Diagnose errors and unrealistic free cash flows in your models.</StepItem>
           </StepContainer>
-        </StepFlex>
-        <Hide sm={true} md={true}><DotLinesImage /></Hide>
-        <StepFlex>
-          <StepContainer >
-            <StepNumber>2</StepNumber>
-            <StepHeading>Authorize any organizations that you want to onboard your members for</StepHeading>
-            <StepContent>
-              <StepItem>Automatically pulls in your organizations and team members instantly.</StepItem>
-              <StepItem>30 second setup.</StepItem>
-            </StepContent>
-          </StepContainer>
-          <Image
-            src={StepTwo}
-          />
-        </StepFlex>
-        <Hide sm={true} md={true}><DotLinesImage style={{ transform: "rotateX(180deg)" }} /></Hide>
-        <StepFlex>
-          <Image
-            src={StepThree}
-          />
-          <StepContainer>
-            <StepNumber>3</StepNumber>
-            <StepHeading>Onboard your new team members directly through GitHub</StepHeading>
-            <StepItem>Create an onboarding process template for each organization.</StepItem>
-            <StepItem>Save your onboarding processes for different teams, i.e backend team, frontend team.</StepItem>
-            <StepItem>Apply your saved onboarding processes to new team members in one click.</StepItem>
-          </StepContainer>
-        </StepFlex>
+        </Box>
       </Box>
-      <GetStartedMailingList />
+      <Box mt="auto" mb={65}>
+        <Text textAlign="center" mb={30} fontWeight="bold" fontSize={28} color="white">
+          Sign up today to start your free trial first when we launch.
+        </Text>
+        <GetStartedMailingList />
+      </Box>
     </Container>
-    <Absolute style={{ width: "100%", height: 300, bottom: "-4%" }} zIndex={-1}>
-      <BackgroundBlue height="100%" width="100%" style={{ transform: "rotateX(180deg)" }} />
+    <Absolute style={{ width: "100%", height: 300, bottom: 0 }} zIndex={-1}>
+      <BackgroundPurple height="100%" width="100%" style={{ transform: "rotateX(180deg)" }} />
     </Absolute>
   </Relative>
 );
