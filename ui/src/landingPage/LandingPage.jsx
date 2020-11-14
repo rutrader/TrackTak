1b94a0c699f9e682455b6879a15164ab4f5c9503
krisolchova <kristina.olchova@yahoo.com>
2020-11-14 15:12:48 +0000

    logo added

--- ui/src/landingPage/LandingPage.jsx
+++ ui/src/landingPage/LandingPage.jsx
@@ -1,21 +1,28 @@
 import React from "react";
-import { Container, Text, Absolute, Relative} from "rebass";
+import { Container, Text, Absolute, Relative } from "rebass";
 import { Box, Flex } from "grid-styled";
 import styled from "styled-components";
 import { ReactComponent as BackgroundPurple } from "../icons/backgroundPurple.svg";
-import { ReactComponent as Check }  from "../icons/check.svg";
-import { ReactComponent as GitHub }  from "../icons/gitHub.svg";
+import { ReactComponent as Check } from "../icons/check.svg";
+import { ReactComponent as GitHub } from "../icons/gitHub.svg";
+import { ReactComponent as TracktakLogo } from "../icons/tracktak.svg";
 import SubscribeMailingList from "./SubscribeMailingList";
 
 const GreenCheck = styled(Check)`
   margin-right: 10px;
   min-width: 30px;
   min-height: 30px;
-  fill: ${({ theme }) => theme.colors.primary}
+  fill: ${({ theme }) => theme.colors.primary};
 `;
 
 const StepItem = ({ children, ...props }) => (
-  <Box is="li" width={["initial", "60%", "100%"]} mx={["5%", "20%", 0]} my={12} {...props}>
+  <Box
+    is="li"
+    width={["initial", "60%", "100%"]}
+    mx={["5%", "20%", 0]}
+    my={12}
+    {...props}
+  >
     <Flex alignItems="center">
       <GreenCheck />
       <Text fontSize={21}>{children}</Text>
@@ -27,42 +34,67 @@ const GetStartedMailingList = () => (
     subscribeText="Sign Me Up"
     submitSucceededMessage={
       <Flex justifyContent="center">
-        Thank you for subscribing!
-        We will notify you when we are live.
+        Thank you for subscribing! We will notify you when we are live.
       </Flex>
     }
   />
 );
 
 const LandingPage = () => (
-  <Relative style={{ height: '100%' }} >
-    <Container pt={20} maxWidth={1100} style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+  <Relative style={{ height: "100%" }}>
+    <Container
+      pt={20}
+      maxWidth={1100}
+      style={{ display: "flex", flexDirection: "column", height: "100%" }}
+    >
       <Box>
-        <Text fontSize={[30, 40]} mt={10}>
-          tracktak
-        </Text>
+        <TracktakLogo />
         <Text fontWeight="bold" fontSize={[40, 60]} mt={30} color="#292929">
           Goodbye, spreadsheets.
           <Box>Hello, automated Discounted Cash Flows.</Box>
         </Text>
-        <Box style={{ display: 'flex' }} mt={60}>
-          <Box style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', flex: 1 }}>
-            <StepItem>Automates your models inputs based on historical and current data.</StepItem>
-            <StepItem>Store all of your model variations in one place instead of 100’s of spreadsheets.</StepItem>
-            <StepItem>Diagnoses errors and unrealistic free cash flows in your models.</StepItem>
+        <Box style={{ display: "flex" }} mt={60}>
+          <Box
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-evenly",
+              flex: 1,
+            }}
+          >
+            <StepItem>
+              Automates your models inputs based on historical and current data.
+            </StepItem>
+            <StepItem>
+              Store all of your model variations in one place instead of 100’s
+              of spreadsheets.
+            </StepItem>
+            <StepItem>
+              Diagnoses errors and unrealistic free cash flows in your models.
+            </StepItem>
           </Box>
           <GitHub style={{ flex: 1 }} height={250} />
         </Box>
       </Box>
       <Box mt="auto" mb={65}>
-        <Text textAlign="center" mb={30} fontWeight="bold" fontSize={28} color="white">
+        <Text
+          textAlign="center"
+          mb={30}
+          fontWeight="bold"
+          fontSize={28}
+          color="white"
+        >
           Sign up today to start your free trial first when we launch.
         </Text>
         <GetStartedMailingList />
       </Box>
     </Container>
     <Absolute style={{ width: "100%", height: 300, bottom: 0 }} zIndex={-1}>
-      <BackgroundPurple height="100%" width="100%" style={{ transform: "rotateX(180deg)" }} />
+      <BackgroundPurple
+        height="100%"
+        width="100%"
+        style={{ transform: "rotateX(180deg)" }}
+      />
     </Absolute>
   </Relative>
 );
--- ui/src/landingPage/StepNumber.jsx
+++ ui/src/landingPage/StepNumber.jsx
@@ -6,16 +6,12 @@ const Step = styled(Circle)`
   margin: 0 auto;
   display: flex;
   justify-content: center;
-  background-color: ${({ theme }) => theme.colors.primary}
+  background-color: ${({ theme }) => theme.colors.primary};
 `;
 
-const StepNumber = ({
-  children,
-}) => (
+const StepNumber = ({ children }) => (
   <Step size={40} fontSize={21}>
-    <Text fontSize={21}>
-      {children}
-    </Text>
+    <Text fontSize={21}>{children}</Text>
   </Step>
 );
 
