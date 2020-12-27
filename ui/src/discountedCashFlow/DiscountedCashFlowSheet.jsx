66825e44a4f5522b421cc46acacc962d69006f5a
Martin Dawson <MDawson@marketaxess.com>
2020-12-27 19:42:36 +0000

    fix re-render table bug

--- ui/src/discountedCashFlow/DiscountedCashFlowSheet.jsx
+++ ui/src/discountedCashFlow/DiscountedCashFlowSheet.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import * as mathjs from "mathjs";
 import { useEffect } from "react";
@@ -69,7 +69,7 @@ const cellUpdate = (data, key, expr) => {
   };
 };
 
-const DiscountedCashFlowSheet = ({ columnWidths }) => {
+const DiscountedCashFlowSheet = (props) => {
   const queryParams = useSelector(selectQueryParams);
   const fundamentals = useSelector((state) => state.fundamentals);
   const costOfCapital = useSelector(selectCostOfCapital);
@@ -81,6 +81,16 @@ const DiscountedCashFlowSheet = ({ columnWidths }) => {
   const [data, setData] = useState(initialData);
   const [showFormulas, setShowFormulas] = useState(false);
   const isOnMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const columnWidths = useMemo(() => {
+    return columns.map((column) => {
+      if (column === "A") {
+        return 220;
+      } else if (showFormulas) {
+        return 200;
+      }
+      return props.columnWidths?.[column] ?? 120;
+    });
+  }, [props.columnWidths, showFormulas]);
 
   const updateCell = useCallback(
     (key, value) => {
@@ -99,65 +109,6 @@ const DiscountedCashFlowSheet = ({ columnWidths }) => {
       setData({ ...data });
     },
     [data]
-  );
-
-  const cellRenderer = useCallback(
-    (rowIndex, columnIndex) => {
-      const column = String.fromCharCode(
-        startColumn.charCodeAt(0) + columnIndex
-      );
-      const row = (rowIndex += 1);
-      const key = column + row;
-      const cell = data[key];
-
-      if (!cell?.value) return <Cell />;
-
-      const { value, type, expr } = cell;
-
-      let node = value;
-
-      const isOutputCell = key === "B37";
-
-      if (type === "percent") {
-        node = <FormatRawNumberToPercent value={value} decimalScale={2} />;
-      }
-      if (type === "million") {
-        node = <FormatRawNumberToMillion value={value} useCurrencySymbol />;
-      }
-      if (type === "currency") {
-        node = <FormatRawNumberToCurrency value={value} />;
-      }
-      if (type === "number") {
-        node = <FormatRawNumber value={value} decimalScale={2} />;
-      }
-
-      let intent = "none";
-
-      if (column === startColumn || rowIndex === 1) {
-        intent = "primary";
-      } else if (showFormulas) {
-        node = expr;
-      }
-
-      if (isOutputCell) {
-        intent = "success";
-      }
-
-      return (
-        <Cell
-          style={{
-            fontSize: theme.typography.fontSize,
-            fontFamily: theme.typography.fontFamily,
-            fontWeight: isOutputCell ? "bold" : "initial",
-            color: "initial",
-          }}
-          intent={intent}
-        >
-          {node}
-        </Cell>
-      );
-    },
-    [data, showFormulas, theme.typography.fontFamily, theme.typography.fontSize]
   );
 
   // TODO: Put all the calculations in redux so we can
@@ -238,6 +189,65 @@ const DiscountedCashFlowSheet = ({ columnWidths }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [queryParams.yearOfConvergence, queryParams.ebitTargetMarginInYearTen]);
 
+  const cellRenderer = useCallback(
+    (rowIndex, columnIndex) => {
+      const column = String.fromCharCode(
+        startColumn.charCodeAt(0) + columnIndex
+      );
+      const row = (rowIndex += 1);
+      const key = column + row;
+      const cell = data[key];
+
+      if (!cell?.value) return <Cell />;
+
+      const { value, type, expr } = cell;
+
+      let node = value;
+
+      const isOutputCell = key === "B37";
+
+      if (type === "percent") {
+        node = <FormatRawNumberToPercent value={value} decimalScale={2} />;
+      }
+      if (type === "million") {
+        node = <FormatRawNumberToMillion value={value} useCurrencySymbol />;
+      }
+      if (type === "currency") {
+        node = <FormatRawNumberToCurrency value={value} />;
+      }
+      if (type === "number") {
+        node = <FormatRawNumber value={value} decimalScale={2} />;
+      }
+
+      let intent = "none";
+
+      if (column === startColumn || rowIndex === 1) {
+        intent = "primary";
+      } else if (showFormulas) {
+        node = expr;
+      }
+
+      if (isOutputCell) {
+        intent = "success";
+      }
+
+      return (
+        <Cell
+          style={{
+            fontSize: theme.typography.fontSize,
+            fontFamily: theme.typography.fontFamily,
+            fontWeight: isOutputCell ? "bold" : "initial",
+            color: "initial",
+          }}
+          intent={intent}
+        >
+          {node}
+        </Cell>
+      );
+    },
+    [data, showFormulas, theme.typography.fontFamily, theme.typography.fontSize]
+  );
+
   // TODO: Add an expand button to see it full screen
   return (
     <Box>
@@ -259,18 +269,13 @@ const DiscountedCashFlowSheet = ({ columnWidths }) => {
           {showFormulas ? "Hide Formulas" : "Show Formulas"}
         </Button>
       </Box>
+      {/* Key: Hack to force re-render the table when formula state changes */}
       <Table
+        key={showFormulas}
         enableGhostCells
         numFrozenColumns={isOnMobile ? 0 : 1}
         numRows={numberOfRows}
-        columnWidths={columns.map((column) => {
-          if (column === "A") {
-            return 220;
-          } else if (showFormulas) {
-            return 200;
-          }
-          return columnWidths?.[column] ?? 120;
-        })}
+        columnWidths={columnWidths}
       >
         {columns.map((column) => {
           return (
