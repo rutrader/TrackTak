dcb2fa6147e2162b8ce27556e168c66cd280b74d
Martin Dawson <MDawson@marketaxess.com>
2021-02-02 13:12:13 +0000

    fix pages crashing

--- ui/src/layout/LayoutFullScreen.jsx
+++ ui/src/layout/LayoutFullScreen.jsx
@@ -1,13 +1,15 @@
 import { Container } from "@material-ui/core";
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { fundamentalsFilter } from "../api/api";
 import Header from "../components/Header";
 import TTTabs from "../components/TTTabs";
 import { getFundamentalsThunk } from "../redux/actions/fundamentalsActions";
+import selectFundamentalsIsLoaded from "../selectors/fundamentalSelectors/selectFundamentalsIsLoaded";
 
 const LayoutFullScreen = ({ children }) => {
+  const isLoaded = useSelector(selectFundamentalsIsLoaded);
   const params = useParams();
   const dispatch = useDispatch();
 
@@ -19,6 +21,8 @@ const LayoutFullScreen = ({ children }) => {
       })
     );
   }, [dispatch, params.ticker]);
+
+  if (!isLoaded) return null;
 
   return (
     <Container maxWidth={false}>
