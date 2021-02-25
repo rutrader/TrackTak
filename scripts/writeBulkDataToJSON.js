f35d4fe85332a6b770e0dcb3eb619a533bcb0c2a
Martin Dawson <MDawson@marketaxess.com>
2021-02-25 17:14:05 +0000

    wrote all fundamentalsData to zip file

--- scripts/writeBulkDataToJSON.js
+++ scripts/writeBulkDataToJSON.js
@@ -37,22 +37,34 @@ const writeBulkFundamentalsDataToJSON = async () => {
       .map((x) => x.Code)
       // US not supported
       .filter((x) => x !== "US")
+      .filter((x) => x !== "BOND")
       .concat(["NYSE", "NASDAQ", "BATS", "AMEX"])
       .filter((x) => jsons.every((jsonName) => !jsonName.includes(x)));
+
+    console.log(exchanges);
 
     exchanges.forEach(async (exchange) => {
       let i = 0;
       // API limits to 1000 requests each time
-      const limit = 1000;
+      const limit = 500;
       let bulkData = [];
       let res;
 
       do {
+        console.log(exchange);
+        console.log("iteration " + i);
+
         const offset = i * limit;
 
-        res = await axios.get(
-          `https://eodhistoricaldata.com/api/bulk-fundamentals/${exchange}?api_token=${apiToken}&fmt=json&offset=${offset}&limit=${limit}`,
-        );
+        try {
+          res = await axios.get(
+            `https://eodhistoricaldata.com/api/bulk-fundamentals/${exchange}?api_token=${apiToken}&fmt=json&offset=${offset}&limit=${limit}`,
+            { timeout: 1000000000 },
+          );
+        } catch (error) {
+          console.error(error);
+          console.log("FAILED: " + exchange);
+        }
         i += 1;
 
         Object.values(res.data).forEach((datum) => {
