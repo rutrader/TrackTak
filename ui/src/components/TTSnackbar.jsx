e68064845da12e4140ee9944ea257cdae300cdf3
Martin Dawson <MDawson@marketaxess.com>
2021-03-23 19:24:17 +0000

    fixed position of snackbar

--- ui/src/components/TTSnackbar.jsx
+++ ui/src/components/TTSnackbar.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-import { Snackbar, Alert, useTheme } from "@material-ui/core";
+import { Snackbar, Alert, makeStyles } from "@material-ui/core";
 import { useDispatch, useSelector } from "react-redux";
 import selectSnackbar from "../selectors/selectSnackbar";
 import { clearMessage } from "../redux/actions/snackbarActions";
 
+const useStyles = makeStyles((theme) => ({
+  snackbarRoot: {
+    top: `${theme.mixins.toolbar.minHeight + 4}px`,
+    "& .MuiSnackbarContent-message": {
+      margin: "0 auto",
+    },
+  },
+}));
+
 const TTSnackbar = () => {
+  const classes = useStyles();
   const snackbar = useSelector(selectSnackbar);
   const dispatch = useDispatch();
-  const theme = useTheme();
 
   const handleClose = (_, reason) => {
     if (reason === "clickaway") return;
@@ -17,11 +26,8 @@ const TTSnackbar = () => {
 
   return (
     <Snackbar
-      sx={{
-        top: `${theme.mixins.toolbar.minHeight + 4}px`,
-        "& .MuiSnackbarContent-message": {
-          margin: "0 auto",
-        },
+      classes={{
+        root: classes.snackbarRoot,
       }}
       autoHideDuration={4000}
       anchorOrigin={{ vertical: "top", horizontal: "center" }}
