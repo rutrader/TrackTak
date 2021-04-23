d9bf6bba10d59a9384492bf68371183765217a91
Martin Dawson <MDawson@marketaxess.com>
2021-04-23 20:45:56 +0100

    test

--- node/webpack.config.cjs
+++ node/webpack.config.cjs
@@ -2,8 +2,6 @@ const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 const CopyPlugin = require("copy-webpack-plugin");
 
-console.log("test");
-
 module.exports = {
   mode: "production",
   entry: ["./src/app.js", "./src/workers/index.js"],
