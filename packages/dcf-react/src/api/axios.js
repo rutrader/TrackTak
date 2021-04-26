37472cbfd7bed6d9c90580b2dd59f4b3c95d2246
Martin Dawson <MDawson@marketaxess.com>
2021-04-26 10:36:16 +0100

    fix baseUrl

--- packages/dcf-react/src/api/axios.js
+++ packages/dcf-react/src/api/axios.js
@@ -1,13 +1,20 @@
 import axios from "axios";
 
-console.log(process.env.GATSBY_API_BASE_URL);
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development";
 
-const baseURL =
+let baseURL =
   process.env.GATSBY_API_BASE_URL ??
   process.env.API_BASE_URL ??
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3001/"
-    : "https://api.tracktak.com/";
+  "https://api.tracktak.com/";
+
+if (activeEnv === "development") {
+  baseURL = "http://localhost:3001/";
+}
+
+if (activeEnv === "staging") {
+  baseURL = "http://api.staging.tracktak.com/";
+}
 
 export default axios.create({
   baseURL,
