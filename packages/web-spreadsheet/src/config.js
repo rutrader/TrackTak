a4f6041383be0f855b97f12fb78ade0aef7ac814
Martin Dawson <MDawson@marketaxess.com>
2021-05-06 19:29:30 +0100

    added web-spreadsheet

---
+++ packages/web-spreadsheet/src/config.js
@@ -0,0 +1,7 @@
+/* global window */
+export const cssPrefix = 'x-spreadsheet';
+export const dpr = typeof window !== "undefined" ? window.devicePixelRatio || 1 : 1;
+export default {
+  cssPrefix,
+  dpr,
+};
