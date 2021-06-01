a3f1c7f8f7877d9a6ecfc9a57acbd1ea85ea2fde
krisolchova <kristina.olchova@yahoo.com>
2021-06-01 18:21:28 +0100

    toolbar redo/undo fix

--- packages/web-spreadsheet/src/component/toolbar/buildRedo.js
+++ packages/web-spreadsheet/src/component/toolbar/buildRedo.js
@@ -1,4 +1,4 @@
 import { makeIconItem } from "./makeIconItem";
 
-export const buildRedo = (eventEmitter) =>
-  makeIconItem(eventEmitter)("redo", "Ctrl+Y");
+export const buildRedo = (eventEmitter, toolbarType) =>
+  makeIconItem(eventEmitter, toolbarType)("redo", "Ctrl+Y");
