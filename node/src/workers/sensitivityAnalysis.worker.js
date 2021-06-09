304fee2342b3396f086cf2a316a85f67ecf4732f
Sam Rose <srose34@gmail.com>
2021-06-09 20:32:28 +0100

    Create Hyperforumla instance before calculating DCF

--- node/src/workers/sensitivityAnalysis.worker.js
+++ node/src/workers/sensitivityAnalysis.worker.js
@@ -1,7 +1,20 @@
+import { HyperFormula } from "hyperformula";
+import { currencySymbolMap } from "currency-symbol-map";
 import { parentPort } from "worker_threads";
 import nodeEndpoint from "comlink/dist/umd/node-adapter";
 import { expose } from "comlink";
 import calculateDCFModel from "../dcfModel/calculateDCFModel";
+
+const getHyperformulaInstance = (sheetsSerializedValues) => {
+  const hyperformula = HyperFormula.buildFromSheets(sheetsSerializedValues, {
+    licenseKey: "05054-b528f-a10c4-53f2a-04b57",
+    currencySymbol: Object.values(currencySymbolMap),
+  });
+  hyperformula.addNamedExpression("TRUE", "=TRUE()");
+  hyperformula.addNamedExpression("FALSE", "=FALSE()");
+
+  return hyperformula;
+};
 
 const sensitivityAnalysisWorker = {
   computeSensitivityAnalysis: (
@@ -9,8 +22,9 @@ const sensitivityAnalysisWorker = {
     existingScope,
     currentScopes,
   ) => {
+    const hyperformula = getHyperformulaInstance(sheetsSerializedValues);
     const values = currentScopes.map((currentScope) => {
-      const model = calculateDCFModel(sheetsSerializedValues, {
+      const model = calculateDCFModel(hyperformula, {
         ...existingScope,
         ...currentScope,
       });
