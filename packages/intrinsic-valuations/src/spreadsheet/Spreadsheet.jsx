67e7ccaf1bf1764f5205386059667d503f5e4ee2
Martin Dawson <MDawson@marketaxess.com>
2021-07-08 09:42:11 +0100

    fixed all problems with the package

--- packages/intrinsic-valuations/src/spreadsheet/Spreadsheet.jsx
+++ packages/intrinsic-valuations/src/spreadsheet/Spreadsheet.jsx
@@ -63,6 +63,7 @@ import getFinancialStatementsData from "./templates/freeCashFlowFirmSimple/data/
 import ExportToExcel, { DCFControlTypography } from "./ExportToExcel";
 import SensitivityAnalysis from "../components/SensitivityAnalysis";
 import Section from "../components/Section";
+import getFormats from "./getFormats";
 
 const requiredInputsId = "required-inputs";
 const dcfValuationId = "dcf-valuation";
@@ -77,39 +78,6 @@ const SpreadsheetLabel = (props) => (
     }}
   />
 );
-
-// Temporary until patterns are in the cell
-// instead of formats
-export const getFormats = (currencySymbol) => {
-  const formats = {
-    "million-currency": {
-      key: "million-currency",
-      title: () => "Million Currency",
-      type: "number",
-      format: "million-currency",
-      label: `${currencySymbol}1,000,000`,
-      pattern: `"${currencySymbol}"#,###.##,,`,
-    },
-    million: {
-      key: "million",
-      title: () => "Million",
-      type: "number",
-      format: "million",
-      label: `1,000,000`,
-      pattern: `#,###.##,,`,
-    },
-    currency: {
-      key: "currency",
-      title: () => "Currency",
-      type: "number",
-      format: "currency",
-      label: `${currencySymbol}10.00`,
-      pattern: `"${currencySymbol}"#,##0.##`,
-    },
-  };
-
-  return formats;
-};
 
 const Spreadsheet = ({ hideSensitivityAnalysis }) => {
   const containerRef = useRef();
