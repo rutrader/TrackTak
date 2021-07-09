7fbecfb37a5740fd6333f1f225d3c8badfe3b9a3
Martin Dawson <MDawson@marketaxess.com>
2021-07-09 15:28:36 +0100

    change from x-spreadsheet to powersheet

--- packages/web-spreadsheet/src/component/calendar.js
+++ packages/web-spreadsheet/src/component/calendar.js
@@ -38,7 +38,7 @@ export default class Calendar {
     this.headerLeftEl = h("div", "calendar-header-left");
     this.bodyEl = h("tbody", "");
     this.buildAll();
-    this.el = h("div", "x-spreadsheet-calendar").children(
+    this.el = h("div", "powersheet-calendar").children(
       h("div", "calendar-header").children(
         this.headerLeftEl,
         h("div", "calendar-header-right").children(
