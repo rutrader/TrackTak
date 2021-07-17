c16e086b988fe092fd7ac92286145b63871dd02c
Sam Rose <srose34@gmail.com>
2021-07-17 19:18:50 +0100

    Remove CSS file for PhoneField

--- ui/src/components/PhoneField.jsx
+++ ui/src/components/PhoneField.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import PhoneInput from "react-phone-input-2";
-import "./PhoneField.css";
+import { Box } from "@material-ui/core";
+import "react-phone-input-2/lib/material.css";
 
 const PhoneField = ({
   value,
@@ -27,7 +28,38 @@ const PhoneField = ({
   };
 
   return (
-    <>
+    <Box sx={{
+      ".react-tel-input .form-control": {
+        width: '100%',
+      },
+      ".special-label": {
+        color: "rgba(0, 0, 0, 0.54)",
+      },
+      ".specialLabelFocus": {
+        color: theme => theme.palette.primary.main,
+      },
+
+      "div.specialLabelFocus > div": {
+        color: theme => theme.palette.primary.main,
+      },
+
+      ".react-tel-input .form-control:focus": {
+        borderColor: theme => theme.palette.primary.main,
+        boxShadow: theme => `0 0 0 1px ${theme.palette.primary.main}`,
+      },
+
+      ".react-tel-input .selected-flag:focus .arrow": {
+        borderTop: theme => `5px solid ${theme.palette.primary.main}`,
+      },
+
+      ".react-tel-input .form-control:focus + div:before": {
+        color: theme => theme.palette.primary.main,
+      },
+
+      ".small": {
+        height: "40px",
+      },
+    }}>
       <PhoneInput
         country={defaultCountry}
         value={value}
@@ -42,7 +74,7 @@ const PhoneField = ({
         inputClass={size === "small" ? "small" : undefined}
         containerClass={isFocussed ? "specialLabelFocus" : undefined}
       />
-    </>
+    </Box>
   );
 };
 
