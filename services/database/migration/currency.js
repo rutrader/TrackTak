a0f8e63cb68ab173a46c56af4d9b5c9ba04772be
MartinDawson <u1356770@gmail.com>
2021-11-18 10:37:49 +0000

    added currencies to file to read before migrating

---
+++ services/database/migration/currency.js
@@ -0,0 +1,75 @@
+import 'dotenv/config'
+import * as database from './client/mongoDbClient'
+import getSymbolFromCurrency from 'currency-symbol-map'
+import axios from 'axios'
+import fs from 'fs'
+
+const baseUrl = 'https://eodhistoricaldata.com/api'
+const fundamentalsUrl = `${baseUrl}/fundamentals`
+
+const convertSubCurrencyToCurrency = currencyCode => {
+  if (currencyCode === 'ILA') {
+    return 'ILS'
+  }
+  if (currencyCode === 'GBX') {
+    return 'GBP'
+  }
+  return currencyCode
+}
+
+const Collections = {
+  SPREADSHEET: 'spreadsheet',
+  POWERSHEET_SPREADSHEET: 'powersheet-spreadsheet'
+}
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
+}
+
+;(async function () {
+  await database.connect()
+
+  const allData = await database.find(Collections.SPREADSHEET)
+  const currencySymbolsMap = {}
+
+  for (let index = 0; index < allData.length; index++) {
+    try {
+      const data = allData[index]
+      const res = await axios.get(
+        `${fundamentalsUrl}/${data.financialData.ticker}`,
+        {
+          params: {
+            api_token: process.env.EOD_HISTORICAL_DATA_API_KEY,
+            filter: 'General::CurrencyCode'
+          }
+        }
+      )
+
+      const requestsRemaining = parseInt(
+        res.headers['x-ratelimit-remaining'],
+        10
+      )
+
+      if (requestsRemaining <= 10) {
+        await sleep(70000)
+      }
+
+      const currency = res.data
+      const currencyCode = convertSubCurrencyToCurrency(currency)
+      const currencySymbol = getSymbolFromCurrency(currencyCode)
+
+      currencySymbolsMap[data.financialData.ticker] = currencySymbol
+
+      console.log(data.financialData.ticker)
+    } catch (error) {
+      console.warn(error)
+      console.log(`error occurred, skipping: ${data.financialData.ticker}`)
+    }
+  }
+
+  fs.writeFileSync(`currencies.json`, JSON.stringify(currencySymbolsMap))
+
+  process.exit(0)
+})()
--- services/database/migration/index.js
+++ services/database/migration/index.js
@@ -1,31 +1,11 @@
 import 'dotenv/config'
 import mapper from './mapper'
 import * as database from './client/mongoDbClient'
-import getSymbolFromCurrency from 'currency-symbol-map'
-import axios from 'axios'
-
-const baseUrl = 'https://eodhistoricaldata.com/api'
-const fundamentalsUrl = `${baseUrl}/fundamentals`
-
-const convertSubCurrencyToCurrency = currencyCode => {
-  if (currencyCode === 'ILA') {
-    return 'ILS'
-  }
-  if (currencyCode === 'GBX') {
-    return 'GBP'
-  }
-  return currencyCode
-}
+import fs from 'fs'
 
 const Collections = {
   SPREADSHEET: 'spreadsheet',
   POWERSHEET_SPREADSHEET: 'powersheet-spreadsheet'
-}
-
-function sleep(ms) {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms)
-  })
 }
 
 ;(async function () {
@@ -44,47 +24,22 @@ function sleep(ms) {
     }
   })
 
+  const currencies = JSON.parse(fs.readFileSync(`currencies.json`))
+
   for (let index = 0; index < mappedData.length; index++) {
-    try {
-      const mappedDatum = mappedData[index]
+    const mappedDatum = mappedData[index]
+    const cells = mappedDatum.sheetData.data.cells
+    const ticker = mappedDatum.financialData.ticker
 
-      const res = await axios.get(
-        `${fundamentalsUrl}/${mappedDatum.financialData.ticker}`,
-        {
-          params: {
-            api_token: process.env.EOD_HISTORICAL_DATA_API_KEY,
-            filter: 'General::CurrencyCode'
-          }
-        }
-      )
+    Object.keys(cells).forEach(key => {
+      const cellData = cells[key]
+      const currencySymbol = currencies[ticker]
 
-      const requestsRemaining = parseInt(
-        res.headers['x-ratelimit-remaining'],
-        10
-      )
-
-      if (requestsRemaining <= 10) {
-        await sleep(70000)
+      if (cellData.dynamicFormat === 'currency') {
+        cells[key].textFormatPattern =
+          currencySymbol + cellData.textFormatPattern
       }
-
-      const currency = res.data
-      const cells = mappedDatum.sheetData.data.cells
-
-      Object.keys(cells).forEach(key => {
-        const cellData = cells[key]
-        const currencyCode = convertSubCurrencyToCurrency(currency)
-        const currencySymbol = getSymbolFromCurrency(currencyCode)
-
-        if (cellData.dynamicFormat === 'currency') {
-          cells[key].textFormatPattern =
-            currencySymbol + cellData.textFormatPattern
-        }
-      })
-    } catch (error) {
-      console.warn(
-        `Error occurred so skipping currency symbol for stock ${mappedDatum.financialData.ticker}: ${error}`
-      )
-    }
+    })
   }
 
   console.log(
@@ -96,7 +51,6 @@ function sleep(ms) {
   }
 
   await database.clearAll(Collections.POWERSHEET_SPREADSHEET)
-
   await database.bulkInsert(Collections.POWERSHEET_SPREADSHEET, mappedData)
 
   process.exit(0)
