81a47d6d937a1ccb3b723988e9cb7c555644e8e4
krisolchova <kristina.olchova@yahoo.com>
2022-01-13 18:27:16 +0000

    yield bond added

--- services/node/src/api/v1/securities/bonds/bondApi.js
+++ services/node/src/api/v1/securities/bonds/bondApi.js
@@ -2,10 +2,30 @@ import alterFromToQuery from '../alterFromToQuery'
 import { getEOD } from '../eodHistoricalData/eodAPI'
 import { getFieldValue } from '../helpers'
 import getEODQuery from '../stocks/getEODQuery'
+import iso3311a2 from 'iso-3166-1-alpha-2'
+import tenYearGovernmentBondYields from './tenYearGovernmentBondYields.json'
 
 export const getGovernmentBond = async (countryISO, query) => {
   const newQuery = alterFromToQuery(getEODQuery(query))
-  const value = await getEOD(`${countryISO}.GBOND`, newQuery)
+  try {
+    const value = await getEOD(`${countryISO}.GBOND`, newQuery)
 
-  return getFieldValue(value, true)
+    return getFieldValue(value, true)
+  } catch (error) {
+    if (error.response.status === 404) {
+      const splits = countryISO.split('10Y')
+
+      if (splits[0]) {
+        const country = iso3311a2
+          .getCountry(splits[0].toUpperCase())
+          .toUpperCase()
+        const tenYearGovernmentBondYield = tenYearGovernmentBondYields.find(
+          x => x.country.toUpperCase() === country
+        ).yield
+
+        return tenYearGovernmentBondYield
+      }
+    }
+    throw error
+  }
 }
